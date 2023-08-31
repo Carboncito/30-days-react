6e9a2e943c869541c3821be682cb1e742f7f7c6a
Carboncito <carboncito760@gmail.com>
2023-08-30 18:59:54 -0600

    init day 4

---
+++ solutions/day-04/index.js
@@ -0,0 +1,14 @@
+const rootElement = document.querySelector('#root')
+
+console.log("wii")
+
+const App = () => {
+    return (
+        <div className="app">
+        <h1>Test</h1>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, rootElement)
+
